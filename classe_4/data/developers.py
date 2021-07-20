5ce2486bb46ed33671b62fb046fdf163c97e0df4
uadson <uadsoncastro@gmail.com>
2021-07-20 17:09:13 -0300

    Create classe_4.py file and data folder in same path

---
+++ classe_4/data/developers.py
@@ -0,0 +1,15 @@
+DEVS = [
+    {'id': 0,
+     'name': 'Uadson',
+     'skills': [
+         'Python', 
+         'Django',
+     ]},
+    
+    {'id': 1,
+     'name': 'Emile',
+     'skills': [
+         'Python',
+         'Flask',
+     ]}
+]
